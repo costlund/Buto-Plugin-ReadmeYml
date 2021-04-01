bf5b17d5bc1d88fab67e1dbf63b9ea39c45fb3d0
Claes Östlund <claes@stenbergit.se>
2021-04-01 14:46:05 +0200

    1.1.0

--- PluginReadmeYml.php
+++ PluginReadmeYml.php
@@ -1,8 +1,11 @@
 <?php
 class PluginReadmeYml{
+  private $calc_date = null;
   function __construct() {
     wfPlugin::includeonce('wf/yml');
     wfPlugin::includeonce('readme/parser');
+    wfPlugin::includeonce('calc/date_v1');
+    $this->calc_date = new PluginCalcDate_v1();
   }
   public function widget_parse($data){
     $data = new PluginWfArray($data);
@@ -111,17 +114,17 @@ class PluginReadmeYml{
       if($i1->get('item')){
         foreach ($i1->get('item') as $v2) {
           $i2 = new PluginWfArray($v2);
-          $li1[] = wfDocument::createHtmlElement('li', array(wfDocument::createHtmlElement('a', $i2->get('name'), array('href' => '#'.$i2->get('id')))));
+          $li1[] = wfDocument::createHtmlElement('li', array(wfDocument::createHtmlElement('a', $this->get_label($i2), array('href' => '#'.$i2->get('id')))));
           if($i2->get('item')){
             $li2 = array();
             foreach ($i2->get('item') as $v3) {
               $i3 = new PluginWfArray($v3);
-              $li2[] = wfDocument::createHtmlElement('li', array(wfDocument::createHtmlElement('a', $i3->get('name'), array('href' => '#'.$i3->get('id')))));
+              $li2[] = wfDocument::createHtmlElement('li', array(wfDocument::createHtmlElement('a', $this->get_label($i3), array('href' => '#'.$i3->get('id')))));
               if($i3->get('item')){
                 $li3 = array();
                 foreach ($i3->get('item') as $v4) {
                   $i4 = new PluginWfArray($v4);
-                  $li3[] = wfDocument::createHtmlElement('li', array(wfDocument::createHtmlElement('a', $i4->get('name'), array('href' => '#'.$i4->get('id')))));
+                  $li3[] = wfDocument::createHtmlElement('li', array(wfDocument::createHtmlElement('a', $this->get_label($i4), array('href' => '#'.$i4->get('id')))));
                 }
                 $li2[] = wfDocument::createHtmlElement('ul', $li3);
               }
@@ -137,26 +140,26 @@ class PluginReadmeYml{
     $element = array();
     foreach ($readme->get() as $v1) {
       $i1 = new PluginWfArray($v1);
-      $element[] = wfDocument::createHtmlElement('h1', $i1->get('name'));
+      $element[] = wfDocument::createHtmlElement('h1', $this->get_label($i1));
       $element[] = wfDocument::createHtmlElement('p', $parser->parse_text($i1->get('description')));
       $element[] = wfDocument::createHtmlElement('ul', $li1);
       if($i1->get('item')){
         foreach ($i1->get('item') as $v2) {
           $i2 = new PluginWfArray($v2);
           $element[] = wfDocument::createHtmlElement('a', null, array('name' => $i2->get('id')));
-          $element[] = wfDocument::createHtmlElement('h2', $i2->get('name'));
+          $element[] = wfDocument::createHtmlElement('h2', $this->get_label($i2));
           $element[] = wfDocument::createHtmlElement('p', $parser->parse_text($i2->get('description')));
           if($i2->get('item')){
             foreach ($i2->get('item') as $v3) {
               $i3 = new PluginWfArray($v3);
               $element[] = wfDocument::createHtmlElement('a', null, array('name' => $i3->get('id')));
-              $element[] = wfDocument::createHtmlElement('h3', $i3->get('name'));
+              $element[] = wfDocument::createHtmlElement('h3', $this->get_label($i3));
               $element[] = wfDocument::createHtmlElement('p', $parser->parse_text($i3->get('description')));
               if($i3->get('item')){
                 foreach ($i3->get('item') as $v4) {
                   $i4 = new PluginWfArray($v4);
                   $element[] = wfDocument::createHtmlElement('a', null, array('name' => $i4->get('id')));
-                  $element[] = wfDocument::createHtmlElement('h4', $i4->get('name'));
+                  $element[] = wfDocument::createHtmlElement('h4', $this->get_label($i4));
                   $element[] = wfDocument::createHtmlElement('p', $parser->parse_text($i4->get('description')));
                 }
               }
@@ -166,5 +169,19 @@ class PluginReadmeYml{
       }
     }
     return $element;
+  }
+  private function get_label($data){
+    $label = $data->get('name');
+    if($data->get('date')){
+      $calc_date = new PluginWfArray($this->calc_date->calcAll($data->get('date'), date('Y-m-d')));
+      if($calc_date->get('days_total')<=30){
+        $innerHTML = $calc_date->get('days_total').' days ago';
+        if($calc_date->get('days_total')==0){
+          $innerHTML = 'Today';
+        }
+        $label .= ' <span class="badge badge-pill badge-success" title="'.$data->get('date').'">'. $innerHTML .'</span>';
+      }
+    }
+    return $label;
   }
 }
--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 Takes yml data like /sys/mercury/readme.yml and parse it to html.
 Also a save parameter to save a .md file at the same time.
 
+
+## Widget parse
 ```
 type: widget
 data:
@@ -11,4 +13,29 @@ data:
   data:
     file: /sys/mercury/readme.yml
     save: /sys/mercury/README.md
+```
+
+Example data.
+```
+readme:
+  name: 'My system documentation'
+  date: '2021-03-28'
+  description: |
+    Description of my software.
+  item:
+    -
+      name: 'Level one'
+      description: ''
+      item:
+        -
+          name: Level two
+          description: |
+            Some text.
+            ```
+            var x = 1;
+            ```
+          item:
+            -
+              name: Level three
+              description: ''
 ```
